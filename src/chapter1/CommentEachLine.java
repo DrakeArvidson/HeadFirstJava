6aff4bbe0cef9485962bdebdc4ac055a46357b53
Drake Arvidson <darvidson@denniskirk.com>
2014-06-06 11:40:50 -0500

    Adding 'Sharpen your pencil' exercise from chapter 1

---
+++ src/chapter1/CommentEachLine.java
@@ -0,0 +1,35 @@
+package chapter1;
+
+public class CommentEachLine {
+	
+	public static void main(String[] args) {
+		System.out.println("This class was created to just complete an exercise in Chapter 1, "
+				+ "commenting what each line does, it doesn't any functioning code");
+	}
+
+	
+//	int size = 27;					//Creates an int variable with the name 'size' and it assigns it a value of 27
+//	String name = "Fido";			//Creates a string variable with the name 'name' and it assigns it the string Fido
+//	Dog myDog = new Dog(name, size);//Creates a Dog object named myDog, it takes in the name and size assigned above
+//	x = size - 5;					//Creates a variable x and assigns it the value of size - 5, 22 in this case
+//	if (x < 15) myDog.bark(8);		//Compares x to 15, if x is less than 15 then make myDog bark 8 times, this will not execute as x is 22
+//	
+//	while (x > 3) {					//Runs this loops as long as x is greater than 3
+//		myDog.play();				//In the loop, tells the myDog object to run the play() method each time through the loop
+//	}
+//	
+//	int[] numList = {2, 4, 6, 8};	//Creates an array of ints named numList, the list contains 2, 4, 6, 8
+//	System.out.print("Hello");		//Prints out "Hello"
+//	System.out.print("Dog: " + name);//Prints out "Dog: Fido"
+//	String num = "8";				//Creates a string variable named num and assigns it the string '8'
+//	int z = Integer.parseInt(num);	//Creates an int variable with the name 'z' and assigns it the int value from the string num, in this case it parses out 8
+//	
+//	try {							//An exception handler, the try block contains code that might result in an exception
+//		readTheFile("myFile.txt");	//Reads the file 'myFile.txt'
+//	}
+//	
+//	catch(FileNotFoundException ex) {//If the code in the try throws an exception, the catch block runs
+//		System.out.print("File not found.");//Prints out "File not found"
+//	}
+	
+}
