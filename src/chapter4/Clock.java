1fcd56b936534774ae236e013da8ccea29b152c8
Drake Arvidson <darvidson@denniskirk.com>
2014-06-19 15:58:33 -0500

    Worked through some of chapter 4.  Committing the work so far.

---
+++ src/chapter4/Clock.java
@@ -0,0 +1,14 @@
+package chapter4;
+//Created for the be the compiler exercise in chapter 4
+public class Clock {
+
+	String time;
+	
+	void setTime(String t) {
+		time = t;
+	}
+	
+	String getTime() {
+		return time;
+	}
+}
